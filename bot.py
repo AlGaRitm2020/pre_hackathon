f82ee0708ded9c74899c83cbe515e2d3ab78fcf1
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2021-08-09 17:24:22 +0400

    added register when bot was activated

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
 from config import TOKEN
-from repo import get_data_from_db
+from repo import get_data_from_db, register
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
@@ -11,6 +11,7 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
+    register(message.from_user.username, message.from_user.id)
     await message.reply("Ты зарегистрировался в системе быстрых оповещений о террористических актах."
                         " Если кто-то укажет тебя в качестве получателя, то в случае террористического "
                         "акта тебе придет сообщение от меня о том кто в опасности и где он находится."
@@ -19,14 +20,22 @@ async def process_start_command(message: types.Message):
                         "")
 
 
+
 @dp.message_handler(commands=['sos'])
 async def process_help_command(message: types.Message):
-    print(message.from_user.username)
     data = get_data_from_db('@' + message.from_user.username)
-    for user_id in data[3]:
-        await bot.send_message(user_id, f'{data[2]} в опастности!')
-    await bot.send_message(message.from_user.id, 'Вы отправили сообщение об опастности своим контактам')
+    list_of_chat_ids = data[3]
+    print(list_of_chat_ids)
+    if list_of_chat_ids:
+        for user_id in data[3]:
+            await bot.send_message(user_id, f'{data[2]} в опастности!')
 
+
+        await bot.send_message(message.from_user.id, 'Вы отправили сообщение об опастности'
+                                                     ' своим контактам')
+    else:
+        await bot.send_message(message.from_user.id,
+                               'Вы не добавили контакты поэтому сообщение SOS никому не отправлено')
 
 @dp.message_handler()
 async def echo_message(msg: types.Message):
--- repo.py
+++ repo.py
@@ -1,6 +1,21 @@
 import sqlite3
 
 
+def register(username, chat_id):
+    username = '@' + username
+
+    con = sqlite3.connect('identifier.sqlite')
+    cur = con.cursor()
+
+    check_request = f"SELECT * FROM users WHERE username = '{username}';"
+    if not cur.execute(check_request).fetchall():
+        print('not found')
+        insert_request = f"INSERT INTO users(username, chat_id) VALUES('{username}', '{chat_id}');"
+        cur.execute(insert_request)
+        con.commit()
+        return True
+
+
 def get_data_from_db(username):
     con = sqlite3.connect('identifier.sqlite')
     cur = con.cursor()
@@ -8,10 +23,12 @@ def get_data_from_db(username):
     request = f"SELECT * FROM users WHERE username = '{username}';"
 
     data = list(cur.execute(request).fetchall()[0])
-    data[3] = data[3].split(', ')
+
+    if data[3]:
+        data[3] = data[3].split(', ')
     print(data)
     return data
 
 
 if __name__ == '__main__':
-    get_data_from_db('@AlGaRitm2020')
+    get_data_from_db('@albert_gareev')
